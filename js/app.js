d7a78c9df2b4bd6c9346e76deade56d709a8026d
unknown <alex.chinchi02@gmail.com>
2024-09-25 20:00:13 +0200

    Avances sobre el fondo y efectos

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ let valorCLick = 1;
 const $galleta = $('#galleta');
 const $imagen = $('#imagen');
 const $galletaClick = $('#galletaClick');
+const $rayos = $('#bgRayos');
+const $body = $('#body');
 
 // EVENTOS
 $galleta.addEventListener('click', puntuar);
@@ -17,18 +19,29 @@ $galleta.addEventListener('click', puntuar);
 // FUNCIONES
 function puntuar(event){
 
-    const { offsetX, offsetY } = event;
+    const x = event.pageX; // Coordenada X del clic
+    const y = event.pageY; // Coordenada Y del clic
     // Actualiza el puntaje 
     puntos += valorCLick; //Habrá que crear en un futuro una funcion
 
+    
+    let destello = document.createElement('div');
+    destello.classList.add('flash');
+    $body.appendChild(destello);
+
     // Animación shake
+    // $destello.classList.remove('flash');
+    // $destello.classList.add('flash');
     $galleta.classList.remove('shake');
     $galleta.classList.add('shake');
     $galletaClick.classList.remove('shake');
     $galletaClick.classList.add('shake');
+    $rayos.classList.add('shake');
     setTimeout(function() {
         $galleta.classList.remove('shake');
         $galletaClick.classList.remove('shake');
+        $rayos.classList.remove('shake');
+        destello.classList.remove('flash');
     }, 400);
 
     // Creo el div
@@ -36,14 +49,15 @@ function puntuar(event){
 
     // Añado el contenido
     nuevoP.innerHTML = `<p>+${valorCLick}</p>`;
+    
 
     // Selecciono el contenedor del div
     $galletaClick.appendChild(nuevoP);
+    $galletaClick.style.left = `${x}px`;
+    $galletaClick.style.top = `${y}px`;
 
-    $galletaClick.style.top = offsetY+"px";
-    $galletaClick.style.left = 500+offsetX+"px";
 
     setTimeout(function(){
         $galletaClick.removeChild(nuevoP);
-    }, 600);
+    }, 2000);
 }
